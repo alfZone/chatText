49082872a0acf56d3d5ca609a118bc0fed1b4383
António Lira Fernandes <47450247+alfZone@users.noreply.github.com>
2022-04-15 22:28:23 +0100

    Update route - to add.php

--- route - to add.php
+++ route - to add.php
@@ -5,7 +5,7 @@ Route::get(['set' => '/users/lista', 'as' => 'users.listOfUsers'], 'ControllerUs
 Route::get('/chat/manutencao', function(){  require _CAMINHO_ADMIN. "chatGerir.php";});
 Route::post('/chat/manutencao', function(){  require _CAMINHO_ADMIN. "chatGerir.php";});
 Route::get(['set' => '/chat/listaMensagens', 'as' => 'users.ListaMensagensTrocadas'], 'ControllerChat@ListaMensagensTrocadas');
-Route::get(['set' => '/chat/addMsg', 'as' => 'users.addMsg'], 'ControllerChat@addMsg');
+Route::post(['set' => '/chat/addMsg', 'as' => 'users.addMsg'], 'ControllerChat@addMsg');
 Route::get(['set' => '/chat/read/{id}', 'as' => 'users.read'], 'ControllerChat@read');
 Route::get(['set' => '/chat/news', 'as' => 'users.newsMsg'], 'ControllerChat@newsMsg');
 Route::get(['set' => '/chat/lastMessageToRead', 'as' => 'users.LastMessageToRead'], 'ControllerChat@LastMessageToRead');
